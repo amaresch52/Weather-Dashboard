3afb5ab08da404bff4969d7eebd047c7adf29175
Tom Hanks <amaresch@gmail.com>
2021-04-26 15:13:23 -0500

    Updated javascript to work with api

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,33 @@
-$(document).ready(function () {
-
-const cityId = document.querySelector(".cityId")
-
-const form = document.querySelector(".search-block");
-
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
 const temp = document.querySelector(".temp");
 const wind = document.querySelector(".wind");
 const humidity = document.querySelector(".humidity");
 const feelsLike = document.querySelector(".feelsLike");
-const date = document.querySelector(".date")
+const cityId = document.querySelector(".cityId")
 
- const citySearch = document.querySelector(".cityName");
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=imperial&appid=4d7d6a520004df223294756e935e5261')
+.then(response => response.json())
+.then(data => {
+  cityId.textContent =  data.name;
+
+  tempData = (data.main.temp).toFixed(1);   
+  temp.textContent = "Temp: "+ tempData;
+  
+  wind.textContent = "Wind Speed: "+ data.wind.speed + " mph";
+  
+  humidity.textContent = "Humidity: "+ data.main.humidity + "%";
+  
+  feelData = data.main.feels_like 
+  feelsLike.textContent = "Feels Like: "+ (feelData).toFixed(1);
+  input.value ="";
 
 
-form.addEventListener(".cityName", e => {
-  e.preventDefault();
-  const citySearch = input.value;
+
 })
 
-const apiKey = "4d7d6a520004df223294756e935e5261";
-const url = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=4d7d6a520004df223294756e935e5261";
-
-date.textContent = ""
-fetch(url)
-.then(function (response) {
-    return response.json();
 })
-.then(function (data) {
-    console.log(data);
-
-
-
-cityId.textContent =  data.name;
-temp.textContent = "Temp: "+ data.main.temp;
-wind.textContent = "Wind: "+ data.wind.speed;
-humidity.textContent = "Humidity: "+ data.main.humidity;
-feelsLike.textContent = "Feels Like: "+ data.main.feels_like;
-
-})})
