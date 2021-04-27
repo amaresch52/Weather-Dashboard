7e5371e99fd3d80ef57c7a12b2db7c392221b007
Tom Hanks <amaresch@gmail.com>
2021-04-27 15:13:06 -0500

    added start of 5day forecast

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,13 @@ var main = document.querySelector('#name');
 var desc = document.querySelector('.desc');
 var clouds = document.querySelector('.clouds');
 var button= document.querySelector('.submit');
-const temp = document.querySelector(".temp");
-const wind = document.querySelector(".wind");
-const humidity = document.querySelector(".humidity");
-const feelsLike = document.querySelector(".feelsLike");
-const cityId = document.querySelector(".cityId")
+var temp = document.querySelector(".temp");
+var wind = document.querySelector(".wind");
+var humidity = document.querySelector(".humidity");
+var feelsLike = document.querySelector(".feelsLike");
+var cityId = document.querySelector(".cityId")
+//const fiveDay = document.querySelector('.fiveDay');
+var weatherForecast = document.querySelector('#weatherForecast');
 
 button.addEventListener('click', function(name){
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=imperial&appid=4d7d6a520004df223294756e935e5261')
@@ -16,7 +18,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=i
   cityId.textContent =  data.name;
 
   tempData = (data.main.temp).toFixed(1);   
-  temp.textContent = "Temp: "+ tempData;
+  temp.textContent = "Temp: "+ tempData + "&degF";
   
   wind.textContent = "Wind Speed: "+ data.wind.speed + " mph";
   
@@ -24,10 +26,32 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=i
   
   feelData = data.main.feels_like 
   feelsLike.textContent = "Feels Like: "+ (feelData).toFixed(1);
+ 
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+input.value+'&units=imperial&cnt=5&appid=4d7d6a520004df223294756e935e5261')
+
+.then(response => response.json())
+.then(data => {
+function forecast(){
+  var fiveDay = "";
+  $.each(data.list, function(index,val) {
+    fiveDay+= "<p>"
+    fiveDay+= "<b>Day " + index + "</b>: "
+    fiveDay+= val.main.temp + "&degF"
+    fiveDay+= "<span> |" + val.weather[0].description + "</span>"
+    fiveDay+= "</p>"
+    console.log(fiveDay);
+    
+  });
+  weatherForecast.innerHTML=(fiveDay)
+
+};
+//function update(){
+//document.getElementbyId("weatherForecast").innerHTML=fiveDay;
+ forecast();
+  console.log(data);
   input.value ="";
+}
 
+)
 
-
-})
-
-})
+})})
